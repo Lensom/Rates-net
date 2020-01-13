d61034bc106147a8ae251159194d95bbab91b8fe
Lensom <magurawork@gmail.com>
2020-01-13 08:37:33 +0200

    Added desktop version

--- project/js/common.js
+++ project/js/common.js
@@ -107,6 +107,17 @@ document.addEventListener('DOMContentLoaded', function(){
 
 });
 
+document.addEventListener('DOMContentLoaded', function(){
+
+    let btn = $('.header__mobile');
+    let body = $('body');
+
+    $(btn).on('click', function() {
+        
+    });
+
+});
+
 
 $(function() {
 	// (Optional) Active an item if it has the class "is-active"	
